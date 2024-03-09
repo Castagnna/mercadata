584745081ea589dd7d34ac3e782e6a467ba80457
Castagnna <daniel.c4stagna@gmail.com>
2024-03-09 14:49:56 -0300

    Setup noop, dry-run

--- etl/jobs/setup.py
+++ etl/jobs/setup.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
+from dateutil.parser import parse as dateparse
 from pyspark.sql import DataFrame
 from tools.spark import start_spark
 from etl.configs import ROOT
@@ -17,9 +19,10 @@ class BaseSetup(ABC):
     ) -> object:
         self.__env = env
         self.__spark = start_spark(app_name, deploy_mode)
-        self.__app_name = app_name
-        self.__deploy_mode = deploy_mode
-        self.__date_ref = date_ref
+        self.__job_start_dttm = datetime.now()
+        self.__date_ref = (
+            self.job_start_dttm if date_ref == "today" else dateparse(date_ref)
+        )
         self.__dry_run = dry_run
         self.__noop = noop
         self.__kwargs = kwargs
@@ -36,6 +39,22 @@ class BaseSetup(ABC):
     @property
     def root(self) -> str:
         return self.__root
+    
+    @property
+    def dry_run(self) -> str:
+        return self.__dry_run
+
+    @property
+    def noop(self) -> str:
+        return self.__noop
+    
+    @property
+    def job_start_dttm(self) -> datetime:
+        return self.__job_start_dttm
+    
+    @property
+    def date_ref(self) -> datetime:
+        return self.__date_ref
 
     @abstractmethod
     def load(self) -> dict:
@@ -47,11 +66,19 @@ class BaseSetup(ABC):
     def transform(self, dataframe: DataFrame) -> DataFrame:
         return dataframe
 
+    @staticmethod
+    def write_noop(output):
+        output.write.format("noop").mode("overwrite").save()
+
     @abstractmethod
     def write(self, output):
-        output.write.format("noop").mode("overwrite").save()
+        if self.noop:
+            self.write_noop(output)
+        else:
+            pass
 
     def run(self):
         loads = self.load()
-        output = self.transform(**loads)
-        self.write(output)
+        if not self.dry_run:
+            output = self.transform(**loads)
+            self.write(output)
