b721e317b5c8a4d5e0ff2af6168949aac794ee49
Daniel CS <56129919+Castagnna@users.noreply.github.com>
2024-03-11 23:46:45 -0300

    feat: testing silver layer (#13)
    * tests silver

---
+++ tests/jobs/silver/DeduplicaVendas/test_deduplica_vendas.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from pyspark.testing.utils import assertDataFrameEqual
+from pyspark.sql.types import StructType, StructField, StringType, DateType
+from etl.jobs.silver.DeduplicaVendas.functions import deduplica_vendas
+
+
+def test_formata_dados(spark_fixture):
+    original = spark_fixture.createDataFrame(
+        [
+            ("V1", "P1", datetime(2024, 1, 1), datetime(2024, 2, 1)),
+            ("V1", "P1", datetime(2024, 1, 2), datetime(2024, 2, 1)),
+            ("V1", "P1", datetime(2024, 1, 2), datetime(2024, 2, 2)),
+        ],
+        StructType(
+            [
+                StructField("COD_ID_VENDA_UNICO", StringType(), True),
+                StructField("COD_ID_PRODUTO", StringType(), True),
+                StructField("DATA_PROCESSAMENTO", DateType(), True),
+                StructField("DATA_DA_COMPRA", DateType(), True),
+            ]
+        ),
+    )
+    transformed = deduplica_vendas(original)
+
+    expected = spark_fixture.createDataFrame(
+        [
+            ("V1", "P1", datetime(2024, 1, 2), datetime(2024, 2, 2)),
+        ],
+        StructType(
+            [
+                StructField("COD_ID_VENDA_UNICO", StringType(), True),
+                StructField("COD_ID_PRODUTO", StringType(), True),
+                StructField("DATA_PROCESSAMENTO", DateType(), True),
+                StructField("DATA_DA_COMPRA", DateType(), True),
+            ]
+        ),
+    )
+
+    assertDataFrameEqual(transformed, expected)
