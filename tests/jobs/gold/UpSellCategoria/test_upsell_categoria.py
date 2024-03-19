b6de6aed4f836f6711b2300fd657c2f877ab1cbd
daniel <daniel.c4stagna@gmail.com>
2024-03-19 20:41:43 -0300

    testing f2 f3

--- tests/jobs/gold/UpSellCategoria/test_upsell_categoria.py
+++ tests/jobs/gold/UpSellCategoria/test_upsell_categoria.py
@@ -1,9 +1,16 @@
 from pyspark.testing.utils import assertDataFrameEqual, assertSchemaEqual
-from pyspark.sql.types import StructType, StructField, StringType, LongType, DoubleType
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    StringType,
+    LongType,
+    DoubleType,
+    ArrayType,
+)
 from etl.jobs.gold.UpSellCategoria.functions import (
     prepara_produtos,
-    # prepara_produtos_por_cliente,
-    # pega_produtos_que_o_cliente_ja_comprou,
+    prepara_produtos_por_cliente,
+    pega_produtos_que_o_cliente_ja_comprou,
     # calcula_qtd_de_transacoes_por_categoria_por_cliente,
     # calcula_top_5_categorias_por_cliente,
     # prepara_vendas_por_produto,
@@ -40,3 +47,74 @@ def test_prepara_produtos(spark_fixture):
     )
 
     assertSchemaEqual(transformed.schema, expected_schema)
+
+
+def test_prepara_produtos_por_cliente(spark_fixture):
+    original = spark_fixture.createDataFrame(
+        [
+            ("L1", "C1", "P1", 2, 10.00, "value1"),
+        ],
+        StructType(
+            [
+                StructField("COD_ID_LOJA", StringType(), True),
+                StructField("COD_ID_CLIENTE", StringType(), True),
+                StructField("COD_ID_PRODUTO", StringType(), True),
+                StructField("qtd_de_transacoes", LongType(), True),
+                StructField("qtd_total_kg", DoubleType(), True),
+                StructField("col1", StringType(), True),
+            ]
+        ),
+    )
+
+    transformed = prepara_produtos_por_cliente(original)
+
+    expected_schema = StructType(
+        [
+            StructField("COD_ID_LOJA", StringType(), True),
+            StructField("COD_ID_CLIENTE", StringType(), True),
+            StructField("COD_ID_PRODUTO", StringType(), True),
+            StructField("qtd_de_transacoes", LongType(), True),
+            StructField("qtd_total_kg", DoubleType(), True),
+        ]
+    )
+
+    assertSchemaEqual(transformed.schema, expected_schema)
+
+
+def test_pega_produtos_que_o_cliente_ja_comprou(spark_fixture):
+    original = spark_fixture.createDataFrame(
+        [
+            ("L1", "C1", "P1"),
+            ("L1", "C1", "P2"),
+            ("L1", "C1", "P2"),
+            ("L1", "C2", "P2"),
+            ("L2", "C1", "P1"),
+            ("L2", "C1", "P2"),
+        ],
+        StructType(
+            [
+                StructField("COD_ID_LOJA", StringType(), True),
+                StructField("COD_ID_CLIENTE", StringType(), True),
+                StructField("COD_ID_PRODUTO", StringType(), True),
+            ]
+        ),
+    )
+
+    transformed = pega_produtos_que_o_cliente_ja_comprou(original)
+
+    expected = spark_fixture.createDataFrame(
+        [
+            ("L1", "C1", ["P2", "P1"]),
+            ("L1", "C2", ["P2"]),
+            ("L2", "C1", ["P2", "P1"]),
+        ],
+        StructType(
+            [
+                StructField("COD_ID_LOJA", StringType(), True),
+                StructField("COD_ID_CLIENTE", StringType(), True),
+                StructField("produtos_ja_comprados", ArrayType(StringType()), True),
+            ]
+        ),
+    )
+
+    assertDataFrameEqual(transformed, expected)
